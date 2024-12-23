f3193230487fe4190781f167e5ce77c64ac33743
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-23 16:45:00 +0530

    Fixed; Render Error

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ const app = express();
 
 //files import
 
-import router from './routes/user.routes';
-import authrouter from './routes/auth.routes';
+import router from './routes/user.routes.ts';
+import authrouter from './routes/auth.routes.ts';
 
 
 //middlewares
