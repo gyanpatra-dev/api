283d045e88af924fe1320d60b88a33d9e744feb8
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2025-01-03 20:39:16 +0530

    fixed; bug

--- src/routes/yearRoutes/yearrouter.ts
+++ src/routes/yearRoutes/yearrouter.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createyear, getallyear } from "../../controller/yearController/year.controller";
 const yearrouter = Router()
 
-yearrouter.get("/:",getallyear);
+yearrouter.get("/",getallyear);
 yearrouter.post("/create",createyear)
 
 
